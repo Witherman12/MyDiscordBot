3bfa88419516b549686a4024ad5e11afb9ec8ba2
Panagiotis Kaltsis <173720543+Witherman12@users.noreply.github.com>
2026-06-27 00:55:12 +0300

    Add files via upload

--- witherbot.py
+++ witherbot.py
@@ -59,7 +59,7 @@ intents.message_content = True
 intents.messages = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 # Φόρτωση των εξωτερικών αρχείων (Cogs)
 @bot.event
